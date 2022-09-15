d07f31533e5bc4feb0487e1d9ebe0b5a965f38ca
Matt Watson <matthew.watson@humanmade.com>
2022-09-15 21:01:57 +0100

    Remove template lock

--- src/edit.js
+++ src/edit.js
@@ -13,7 +13,6 @@ export default function Edit( { attributes: { endpoint }, setAttributes } ) {
 		template: [
 			[ 'external-template/external-template' ]
 		],
-		templateLock: "all"
 	} );
 
 	/**
